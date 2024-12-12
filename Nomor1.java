8d4b705a78048c2922a6ff743874ec591d6880bc
BBaguss <113284072+BBguss@users.noreply.github.com>
2024-12-12 13:43:45 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Nomor1 {
       public static void bilN(int N) {
-        for (int i = 0; i <= N; i++) {
+       for (int i = 0; i <= N; i++) {
             if (i == 0) {
-                System.out.print("0 "); 
+                System.out.print("0");
             } else if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("FizzBuzz ");
+                System.out.print(" FizzBuzz");
             } else if (i % 3 == 0) {
-                System.out.print("Fizz ");
+                System.out.print(" Fizz");
             } else if (i % 5 == 0) {
-                System.out.print("Buzz ");
+                System.out.print(" Buzz");
             } else {
-                System.out.print(i + " ");
+                System.out.print(" " + i);
             }
-        }           
+        }
+        System.out.println(); 
+    }       
         }
       
     public static void main(String[] args){
@@ -22,8 +24,6 @@ public class Nomor1 {
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
         bilN(N);
-
-        System.out.println("");
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
