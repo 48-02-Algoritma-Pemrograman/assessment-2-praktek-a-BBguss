6a71c93516b5407bd85a88001acf660512f14a96
BBaguss <113284072+BBguss@users.noreply.github.com>
2024-12-12 13:10:12 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -3,20 +3,16 @@ import java.util.Scanner;
 public class Nomor1 {
      public static void bilN(int N) {
        for (int i = 0; i <= N; i++) {
+            // Cek kondisi FizzBuzz
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("FizzBuzz");
+                System.out.print("FizzBuzz ");
             } else if (i % 3 == 0) {
-                System.out.print("Fizz");
+                System.out.print("Fizz ");
             } else if (i % 5 == 0) {
-                System.out.print("Buzz");
+                System.out.print("Buzz ");
             } else {
-                System.out.print(i);
+                System.out.print(i + " ");
             }
-            if (i < N) {
-                System.out.print(" ");
-            }
-        }
-        System.out.println();
         }
 
     public static void main(String[] args){
