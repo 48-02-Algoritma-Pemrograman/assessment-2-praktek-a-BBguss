bd36273e1c39f58e868e8c4187694d50aee76767
BBaguss <113284072+BBguss@users.noreply.github.com>
2024-12-12 13:34:22 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Nomor1 {
       public static void bilN(int N) {
         for (int i = 0; i <= N; i++) {
-            // Cek kondisi FizzBuzz
             if (i == 0) {
-                System.out.print("0 "); // Tampilkan 0 secara eksplisit
+                System.out.print("0 "); 
             } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FizzBuzz ");
             } else if (i % 3 == 0) {
@@ -25,7 +24,7 @@ public class Nomor1 {
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
         bilN(N);
-        System.out.println(" ");
+        System.out.println("");
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
